7c3a943e223bcc86aaf53bdd99250dbb12a5e037
unknown <glorianiyonkuru7@gmail.com>
2021-12-14 15:36:03 +0200

    changes

--- ussd/views.py
+++ ussd/views.py
@@ -30,35 +30,29 @@ def ussdApp(request):
             response += "1. report for crop updates \n"
         elif text == '1':
             response = "CON report for crop updates \n"
-            fetchproduct=CropModels.objects.all()
-            response="CON report in for crop updates \n"
-            for product in fetchproduct:
-                response +=""+str(product.id)+"."+str(product)+ "\n"
-                if text=='1':
-                    response="END you will be sent sms alert for the report you requested for"
-                  
-        #     response += "1. sweet potatoes \n"
-        #     response += "2. maize \n"
-        #     response += "3.ibindi"
-        # elif text == '1*1':
-        #     product="sweet potatoes"
-        #     response = "CON report on sweet potatoes yeild "+str(product)+"\n"
-        # elif category =='1*1' and int(len(level)) == 3 and str(level[2]) in  str(level):
-        #     response = "END you will be sent sms alert for the report you requested for\n"
+            response += "1. sweet potatoes \n"
+            response += "2. maize \n"
+            response += "3.ibindi"
+        elif text == '1*1':
+            product="sweet potatoes"
+            response = "CON report on sweet potatoes yeild "+str(product)+"\n"
+        elif category =='1*1' and int(len(level)) == 3 and str(level[2]) in  str(level):
+            response = "END you will be sent sms alert for the report you requested for\n"
 
-        # elif text == '1*2':
-        #     product ="maize"
-        #     response ="CON report on maize yeild "+str(product)+"\n"
-        # elif category =='1*2' and int(len(level)) == 3 and str(level[2]) in  str(level):
-        #     response = "END you will be sent sms alert for the report you requested for\n"
+        elif text == '1*2':
+            product ="maize"
+            response ="CON report on maize yeild "+str(product)+"\n"
+        elif category =='1*2' and int(len(level)) == 3 and str(level[2]) in  str(level):
+            response = "END you will be sent sms alert for the report you requested for\n"
         # elif text=='1*3':
         #     response="CON report in for crop updates"
         #     fetchproduct=CropModels.objects.all()
         #     response="CON report in for crop updates"
         #     for product in fetchproduct:
         #         response +=""+str(product.id)+"."+str(product)+ "\n"
+
         else:
-            response = "END you will be sent sms alert for the report you requested for\n"
+            response = "END Ukanze ibitaribyo, ongera mukanya"
         return HttpResponse(response)
     else:
         return HttpResponse('we are on ussd app')
